417d5e10dcb0c22ba0aa92efe8b9c5fd9d865d5d
Simon Ball <simonball245@gmail.com>
2017-09-19 14:46:25 +0100

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,13 +8,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     assert_no_difference 'User.count' do
 
-      post signup_path, params: { user: { name: '',
+      post signup_path, params: { user: { name:                  '',
 
-                                         email: 'user@invalid',
+                                          email:                 'user@invalid',
 
-                                         password:              'foo',
+                                          password:              'foo',
 
-                                         password_confirmation: 'bar' } }
+                                          password_confirmation: 'bar' } }
 
     end
 
@@ -26,6 +26,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     assert_select 'form[action="/signup"]'
 
+  end
+
+  test 'valid signup information' do
+
+    get signup_path
+
+    assert_difference 'User.count', 1 do
+
+      post signup_path, params: { user: { name:                  'valid user',
+
+                                          email:                 'user@valid.com',
+
+                                          password:              '123456',
+
+                                          password_confirmation: '123456' } }
+
+    end
+
+    follow_redirect!
+
+    assert_template 'users/show'
+
+    assert_not flash.empty?
   end
 
 end
