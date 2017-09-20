5e0e5165250aa115df31e24df999a00df0d783bf
Simon Ball <simonball245@gmail.com>
2017-09-20 09:55:12 +0100

    Implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,5 +18,13 @@ module SessionsHelper
 
   end
 
+  def log_out
+
+    session.delete(:user_id)
+
+    @current_user = nil
+
+  end
+
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../../config/environment', __FILE__ )
+require File.expand_path('../../config/environment', __FILE__)
 
 require 'rails/test_help'
 
@@ -9,18 +9,20 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 
-
-
 class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
 
   fixtures :all
-  
+
   include ApplicationHelper
 
+  # Returns true if a test user is logged in.
 
+  def is_logged_in?
 
-  # Add more helper methods to be used by all tests here...
+    !session[:user_id].nil?
+
+  end
 
 end
